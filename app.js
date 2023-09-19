8c87cccf41927873729fb0a44d17f4c2fd27178e
Gian Comunello <gianmcomu@gmail.com>
2023-09-19 18:50:40 -0300

    agregando javascript

---
+++ app.js
@@ -0,0 +1,14 @@
+const cambiarComentarios = document.getElementById("comentarios");
+
+function cambiarPrimerLi() {
+  cambiarComentarios.innerHTML =
+    "<h3>Las zapatillas de tenis son alucinantes y super comodas, me hicieron amar este deporte ademas de hacerme sentir que juego como federer, sinceramente se lleva mis dieces.</h3>";
+}
+function cambiarSegundoLi() {
+  cambiarComentarios.innerHTML =
+    "<h3>A mi las raquetas que compre me parecieron super comodas y vertatiles ademas de golpear la pelota con mejor direccion, tambien aumento la fuerza de mi disparo</h3>";
+}
+function cambiarTercerLi() {
+  cambiarComentarios.innerHTML =
+  "<h3>Gran comodidas en las zapatillas, buena calidad de raquetas y la vincha que me compre esta espectacular</h3>";
+}
